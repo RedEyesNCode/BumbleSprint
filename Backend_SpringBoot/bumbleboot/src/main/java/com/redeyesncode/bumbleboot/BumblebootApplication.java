5edeb06262706b22dbada3c9df2b4a4e136c381b
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-03-05 21:51:03 +0530

    Update setup for SpringBoot and Android-Compose.

---
+++ Backend_SpringBoot/bumbleboot/src/main/java/com/redeyesncode/bumbleboot/BumblebootApplication.java
@@ -0,0 +1,13 @@
+package com.redeyesncode.bumbleboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumblebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumblebootApplication.class, args);
+	}
+
+}
---
+++ Backend_SpringBoot/bumbleboot/src/test/java/com/redeyesncode/bumbleboot/BumblebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redeyesncode.bumbleboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumblebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
